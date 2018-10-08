786151f7662dae68f0547cbfec1d112565e0a4e0
paresh2190 <paresh@experiencecommerce.com>
2018-10-08 13:33:32 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,38 @@ $(document).ready(function() {
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
+
 		},
 	});
+
+	// var wihH = $(window).width()
+
+	$('.swiper-button-next').on('click', function(event) {
+		TweenMax.to('.item.t2', 1, {
+			right: '900px',
+			bottom: '300px',
+			rotation: 200,
+		});
+		TweenMax.to('.item.t1', 1, {
+			right: '900px',
+			bottom: '800px',
+			rotation: 200,
+		});
+
+
+	});
+
+	$('.swiper-button-prev').on('click', function(event) {
+		TweenMax.to('.item.t2', 1, {
+			right: '0px',
+			bottom: '00px',
+			rotation: 0,
+		});
+		TweenMax.to('.item.t1', 1, {
+			right: '900px',
+			bottom: '300px',
+			rotation: 0,
+		});
+	});
+
 });
